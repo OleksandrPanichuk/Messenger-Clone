c7f698050e98daf4d0e8d94a08b7dac215e67a97
OleksandrPanichuk <sashapanichuk@gmail.com>
2023-09-16 11:31:48 +0300

    Another Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Provider from '@/components/Provider'
-import ActiveStatus from '@/components/ActiveStatus'
+import ActiveStatus from '@/components/ui/ActiveStatus'
 
 const inter = Inter({ subsets: ['latin'] })
 
